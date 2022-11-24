bda9ec88ee9bcca3188942300da11ed1815c569e
PinJhih <isaacwang29@gmail.com>
2022-11-24 15:48:47 +0800

    feat: Initiate necessary objects in main program.

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class PhoneBook {
+	private static Scanner stdin = new Scanner(System.in);
+	private static Config config = new Config();
+	private static AccountMgr accountMgr = new AccountMgr(config);
+	private static ContactMgr contactMgr = new ContactMgr();
+	private static CatMgr catMgr = new CatMgr();
+
+	public static void main(String[] args) {
+	}
 }
