08ca8f90951ce3684758f17103f9c45f31c5633c
PinJhih <isaacwang29@gmail.com>
2022-11-24 15:38:43 +0800

    feat: Add AccountMgr to read the account.txt and provide login feature.

---
+++ AccountMgr.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountMgr {
+	String verifyStr;
+	Map<String, String> accounts = new HashMap<>();
+
+	public AccountMgr(Config config) {
+		FileMgr source = new FileMgr("account.txt");
+		ArrayList<String> data = source.getData();
+
+		for (String account : data) {
+			String[] tokens = account.split(" ");
+			accounts.put(tokens[0], tokens[1]);
+		}
+
+		verifyStr = config.getVerifyString();
+	}
+
+	public boolean login(String acc, String pw, String vs) {
+		if (!vs.equals(verifyStr))
+			return false;
+		if (!accounts.keySet().contains(acc))
+			return false;
+		return accounts.get(acc).equals(pw);
+	}
+}
--- Test.java
+++ Test.java
@@ -1,9 +1,7 @@
 public class Test {
 	public static void main(String[] args) {
-		ContactMgr mgr = new ContactMgr();
-
-		for (Contact c : mgr.getContacts()) {
-			c.print();
-		}
+		Config config = new Config();
+		AccountMgr mgr = new AccountMgr(config);
+		System.out.println(mgr.login("cis", "134", "12345"));
 	}
 }
