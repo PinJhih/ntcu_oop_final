c6f02deeb495670a08dfac8817f2a025a479aaf6
PinJhih <isaacwang29@gmail.com>
2022-11-25 10:47:24 +0800

    feat: Let FileMgr be able to write data to files

--- FileMgr.java
+++ FileMgr.java
@@ -1,25 +1,43 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class FileMgr {
-	private ArrayList<String> data;
+	File file;
+	String path;
 
 	public FileMgr(String path) {
+		this.path = path;
+		this.file = new File(path);
+	}
+
+	public ArrayList<String> getData() {
+		ArrayList<String> res = new ArrayList<>();
 		try {
-			FileReader reader = new FileReader(path);
+			FileReader reader = new FileReader(file);
 			BufferedReader source = new BufferedReader(reader);
-			data = new ArrayList<>();
+			res = new ArrayList<>();
 			for (String l = source.readLine(); l != null; l = source.readLine()) {
-				data.add(l);
+				res.add(l);
 			}
 			source.close();
 		} catch (Exception e) {
 			System.out.println("Error: Can not read data from " + path);
 		}
+		return res;
 	}
 
-	public ArrayList<String> getData() {
-		return data;
+	public void setData(ArrayList<String> data) {
+		try {
+			FileWriter writer = new FileWriter(file);
+			writer.write(""); // clear file
+			for (String line : data)
+				writer.append(line + "\n");
+			writer.close();
+		} catch (Exception e) {
+			System.out.println("Error: Can not write data to " + path);
+		}
 	}
 }
